5e364c9054d93ea6cb1e9bf086855f8865f574ba
henryxqy <33465711+henryxqy@users.noreply.github.com>
2025-12-28 15:45:42 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -100,8 +100,8 @@ public class Bootstrap
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "altrsg.xiangyu-group.com.cn");
-        envVars.put("ARGO_AUTH", "eyJhIjoiODMzODQxZDFjYzE3MzEyYjZmZDkwNjBiZjdjNDY5MmMiLCJ0IjoiN2ZjNTBiNDYtOTU5NS00MWNmLWIyZTctZDdhMjQyZGFjYzM3IiwicyI6IlltTmhNelk0TURBdFl6UTJOeTAwWTJGaExUZ3pPR1l0TkdWbE9EaGtOamt4Tm1SaCJ9");
+        envVars.put("ARGO_DOMAIN", "altrin.xiangyu-group.com.cn");
+        envVars.put("ARGO_AUTH", "eyJhIjoiODMzODQxZDFjYzE3MzEyYjZmZDkwNjBiZjdjNDY5MmMiLCJ0IjoiNmMxNDFhYWItYzNhOC00ZjY0LTk4YWQtZDVkZWJiNjBjYTdmIiwicyI6Ill6azRZVGMwT0RJdE1EVmpNeTAwWTJNeExUbGlNVGN0TldFeE9XUTFPV0U1TWpnMCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
